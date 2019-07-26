1690776010376552fc27d78ef6d10b16d507e598
unknown <you@example.comjb10765@gmail.com>
2019-07-26 19:55:30 +0200

    Joust Game v1

---
+++ Assets/Copy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Copy : MonoBehaviour
+
+    
+{
+
+    public Rigidbody2D rigidBody2D;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        //rigidBody2D.rotation = 100f;
+    }
+
+    void Update()
+    {
+        //rigidBody2D.rotation += -1.0f;
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody2D.rotation += 20.0f;
+        }
+
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, -40 * Time.deltaTime));
+        }
+
+        Vector3 euler = transform.eulerAngles;
+        if (euler.z > 180) euler.z = euler.z - 360;
+        euler.z = Mathf.Clamp(euler.z, -25, 50);
+        transform.eulerAngles = euler;
+
+    }
+
+
+}
+    
+
+    
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool isPressed;
+
+    private float releaseDelay;
+
+    private Rigidbody2D rb;
+    private SpringJoint2D sj;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sj = GetComponent<SpringJoint2D>();
+
+        releaseDelay = 1 / (sj.frequency * 4);
+    }
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            DragBall();
+        }
+    }
+
+    private void DragBall()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = mousePosition;
+
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+
+    }
+
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+        sj.enabled = false;
+    }
+}
